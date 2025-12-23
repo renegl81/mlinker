ac0f6eb675090013901a12e0b67b3a5fb4e14756
René González <renegl81@gmail.com>
2025-12-23 17:46:56 +0100

    Auth validation messages updated and added to en/auth.php

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de lenguaje de autenticación
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas se utilizan durante la autenticación para varios
+    | mensajes que necesitamos mostrar al usuario.
+    |
+    */
+
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor intente nuevamente en :seconds segundos.',
+
+];
